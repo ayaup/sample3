a0761250e0d51a1ad9addbd47fc00eb8b72bcaca
ayuper <ayaup@qq.com>
2018-01-18 16:11:02 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,21 @@ class UsersController extends Controller
 
 {
 
+    public function followings(User $user)
+        {
+            $users = $user->followings()->paginate(30);
+            $title = '关注的人';
+            return view('users.show_follow', compact('users', 'title'));
+        }
+
+        public function followers(User $user)
+        {
+            $users = $user->followers()->paginate(30);
+            $title = '粉丝';
+            return view('users.show_follow', compact('users', 'title'));
+        }
+
+
     public function __construct()
         {
             $this->middleware('auth', [
